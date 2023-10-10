eb1b0f5f11c1fd01ad3c4c8e7d5501d851b745bb
Proprogam <k.kartal9595@gmail.com>
2023-10-10 21:28:56 +0300

    styles added

---
+++ app/add-product/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddProducts = () => {
+  return <div className="bg-slate-500 text-red-400">AddProducts</div>;
+};
+
+export default AddProducts;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="bg-black text-orange-600">Homasdsaasdasde</div>;
 };
 
 export default Home;
