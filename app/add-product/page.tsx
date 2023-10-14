17d561ed67588ed9098ba0b1213f040ddfb751fd
Proprogam <k.kartal9595@gmail.com>
2023-10-14 12:25:47 +0300

    server action created

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,5 +1,5 @@
 
-import {prisma} from "@/lib/db/prisma";
+import prisma from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 import React from "react";
 
--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,9 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+// /lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+declare global {
+  var prisma: PrismaClient
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export default prisma
